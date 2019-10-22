150f34ab195d0ef58c06e85ab4cc3204fed89015
sadie <sgill@flowwest.com>
2019-10-22 16:04:34 -0700

    init rough component

---
+++ wq-app/src/components/Creek.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header, Button } from 'semantic-ui-react';
+
+const Creek = () => {
+
+  return (
+    <div>
+      <Header as='h1'>Creek</Header>
+      <Button>Creek</Button>
+    </div>
+  );
+
+}
+
+export default Creek;
---
+++ wq-app/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header, Button } from 'semantic-ui-react';
+
+const Home = () => {
+
+  return (
+    <div>
+      <Header as='h1'>Home</Header>
+      <Button>Home</Button>
+    </div>
+  );
+
+}
+
+export default Home;
