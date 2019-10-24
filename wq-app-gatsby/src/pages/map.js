df41061e1c076e75815f62f440a4121d7c4c9b8a
sadie <sgill@flowwest.com>
2019-10-24 16:27:15 -0700

    add favicon and fix helmet

--- wq-app-gatsby/src/pages/map.js
+++ wq-app-gatsby/src/pages/map.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const Map = () => (
-  <Layout></Layout>
+  <Layout>
+    <SEO title="Map" />
+  </Layout>
 )
 
 export default Map
