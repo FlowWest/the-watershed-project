c95431578ca52bb4d52fac41b0bc2d77a364a22b
sadie <sgill@flowwest.com>
2019-10-24 19:43:02 -0700

    prep json data scripts

---
+++ data-raw/combine_wq_categories_features.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(jsonlite)
+
+wq_categories <- read_csv('data-raw/wq_categories.csv') 
+
+wq_features <- read_csv('data-raw/wq_features.csv') %>% 
+  rename(feature_description = description)
+
+data <- wq_features %>% 
+  left_join(wq_categories) 
+
+data %>% 
+  group_by(category, description) %>% 
+  nest(.key = "features") %>% 
+  toJSON(pretty = TRUE) %>% 
+  write_json('wq-app-gatsby/src/data/wq_categories_features.json')
