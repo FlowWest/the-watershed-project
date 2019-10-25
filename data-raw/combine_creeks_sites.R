99a3bbe74af27f32f6c75f2790f20dac1c267a89
sadie <sgill@flowwest.com>
2019-10-25 14:31:53 -0700

    add lat long of sites

--- data-raw/combine_creeks_sites.R
+++ data-raw/combine_creeks_sites.R
@@ -4,10 +4,12 @@ library(jsonlite)
 creeks <- read_csv('data-raw/creeks.csv')
 creeks_to_sites <- read_csv('data-raw/creeks_to_sites.csv')
 sites <- read_csv('data-raw/sites.csv')
+site_locations <- read_csv('data-raw/site_locations.csv')
 
 creeks_to_sites %>% 
   left_join(creeks) %>% 
   left_join(sites) %>% 
+  left_join(site_locations) %>% 
   group_by(creek_name, creek_description, creek_id) %>% 
   nest(.key = 'sites') %>% 
   toJSON() 
