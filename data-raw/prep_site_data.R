35fded4ae4b7c66e7a0947c1bed111e589f9f7fb
sadie <sgill@flowwest.com>
2019-10-29 16:35:33 -0700

    prep data for plots

---
+++ data-raw/prep_site_data.R
@@ -0,0 +1,29 @@
+library(readxl)
+library(tidyverse)
+library(jsonlite)
+
+excel_sheets('data-raw/Data_for_CEDEN_-_Sep_2018_to_Apr_2019.xlsx')
+
+habitat_results_raw <- read_excel('data-raw/Data_for_CEDEN_-_Sep_2018_to_Apr_2019.xlsx', 'HabitatResults')
+
+glimpse(habitat_results_raw)
+
+habitat_results_raw %>% 
+  select(StationCode, SampleDate, AnalyteName, VariableResult) %>% 
+  filter(VariableResult != "Not Recorded") %>% 
+  group_by(StationCode, AnalyteName) %>% 
+  nest() %>% 
+  toJSON() #%>% 
+  # write_json('wq-app-gatsby/src/data/habitat_data.json')
+
+field_results_raw <- read_excel('data-raw/Data_for_CEDEN_-_Sep_2018_to_Apr_2019.xlsx', 'FieldResults')
+glimpse(field_results_raw)
+
+field_results_raw %>% 
+  select(StationCode, SampleDate, AnalyteName, UnitName, Result) %>% 
+  mutate(AnalyteName = replace(AnalyteName, AnalyteName == 'Oxygen, saturation', 'Oxygen, Saturation')) %>% 
+  group_by(StationCode, AnalyteName, UnitName) %>% 
+  nest() %>% 
+  toJSON()
+
+field_results_raw$AnalyteName %>% unique
