1c3c1dc37c68fa8ced1a9675970aa2632c5a3597
sadie <sgill@flowwest.com>
2019-10-30 16:35:39 -0700

    [add] image galler

--- wq-app-gatsby/src/templates/site.js
+++ wq-app-gatsby/src/templates/site.js
@@ -1,19 +1,39 @@
 import React, { Fragment, useState } from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import { Header, Grid, GridColumn, Tab, Dropdown } from "semantic-ui-react"
+import img1 from '../images/Monitoring-Walnut-Creek-crop-1012x1024.jpg'
+import img2 from '../images/20180502_105406.jpg'
+import img3 from '../images/20180816_111841.jpg'
+import img4 from '../images/20180824_121105.jpg'
+
+import {
+  Grid,
+  GridColumn,
+  Tab,
+  Dropdown,
+  Divider,
+  Image,
+  Container,
+} from "semantic-ui-react"
+
 import Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
 import moment from "moment"
 
 export default ({ data, pageContext }) => {
+  const defaultAnalyte = "Temperature"
+  const [analyte, setAnalyte] = useState(defaultAnalyte)
+  const [selectedImage, setImage] = useState(img1)
 
-  const defaultAnalyte = 'Temperature'
-  const [ analyte, setAnalyte ] = useState(defaultAnalyte);
   const sitesData = data.allCreekSiteJson.edges[0].node
+  const [siteData] = sitesData.sites.filter(
+    site => site.site_id === pageContext.siteID
+  )
+
   const sitesWQData = data.allFieldDataJson.edges
-  const plotData = sitesWQData.filter(edge => (edge.node.AnalyteName === analyte))[0].node
-  const [ siteData ] = sitesData.sites.filter(site => site.site_id === pageContext.siteID)
+  const plotData = sitesWQData.filter(
+    edge => edge.node.AnalyteName === analyte
+  )[0].node
 
   const analyteOptions = sitesWQData.map(edge => ({
     key: edge.node.AnalyteName,
@@ -48,66 +68,79 @@ export default ({ data, pageContext }) => {
     ],
   }
 
-  const handleAnalyteChange = (e) => {
-    e.preventDefault();
-    return setAnalyte(e.target.textContent);
+  const handleAnalyteChange = e => {
+    e.preventDefault()
+    return setAnalyte(e.target.textContent)
   }
 
   const panes = [
     {
       menuItem: "Within",
       render: () => (
-          <Tab.Pane attached={false}>
-            <div>
-              <h3>Select Water Quality Feature</h3>
-              <Dropdown
-                placeholder="Water Quality Feature"
-                search
-                selection
-                options={analyteOptions}
-                onChange={handleAnalyteChange}
-                defaultValue={defaultAnalyte}
-              />
-            </div>
-            <HighchartsReact highcharts={Highcharts} options={plotOptions} />
-          </Tab.Pane>
-        ),
+        <Tab.Pane attached={false}>
+          <div>
+            <h3>Select Water Quality Feature</h3>
+            <Dropdown
+              placeholder="Water Quality Feature"
+              search
+              selection
+              options={analyteOptions}
+              onChange={handleAnalyteChange}
+              defaultValue={defaultAnalyte}
+            />
+          </div>
+          <HighchartsReact highcharts={Highcharts} options={plotOptions} />
+        </Tab.Pane>
+      ),
     },
     {
       menuItem: "Between",
-      render: () => (<Tab.Pane attached={false}>Tab 2 Content</Tab.Pane>),
+      render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane>,
     },
   ]
 
+  // siteData.images ['1.jpg', '2.jpg', '3.jpg']
+  // state defautls to siteData.images[0]
+  // group 1, 2, 3 and on click update state
   return (
     <Layout>
-      <Grid>
-        <GridColumn width={6}>
-          <Header as="h2">{siteData.name}</Header>
-          <p>{siteData.description}</p>
-          <ul>
-            {sitesData.sites
-              .filter(site => site.site_id !== pageContext.siteID)
-              .map(site => {
-                return (
-                  <Fragment key={site.site_id}>
-                    <li>
-                      <Link to={`site/${site.site_id}`}>{site.name}</Link>
-                    </li>
-                  </Fragment>
-                )
-              })}
-          </ul>
-          <p>
+      <Container>
+        <Grid>
+          <GridColumn width={6}>
+            <h2>{siteData.name}</h2>
+            <Image src={selectedImage} alt="image"></Image>
+            <Divider hidden />
+            <Image.Group size="tiny">
+              <Image src={img1} onClick={()=>(setImage(img1))}/>
+              <Image src={img2} onClick={()=>(setImage(img2))}/>
+              <Image src={img3} onClick={()=>(setImage(img3))}/>
+              <Image src={img4} onClick={()=>(setImage(img4))}/>
+            </Image.Group>
+            <Divider hidden />
+            <p>{siteData.description}</p>
             <Link to={`/creek/${sitesData.creek_id}`}>
               Go back to {sitesData.creek_name}
             </Link>
-          </p>
-        </GridColumn>
-        <GridColumn width={10}>
-          <Tab menu={{ secondary: true, pointing: true }} panes={panes}></Tab>
-        </GridColumn>
-      </Grid>
+            <h3>Other Sites on {sitesData.creek_name}</h3>
+            <ul>
+              {sitesData.sites
+                .filter(site => site.site_id !== pageContext.siteID)
+                .map(site => {
+                  return (
+                    <Fragment key={site.site_id}>
+                      <li>
+                        <Link to={`site/${site.site_id}`}>{site.name}</Link>
+                      </li>
+                    </Fragment>
+                  )
+                })}
+            </ul>
+          </GridColumn>
+          <GridColumn width={10}>
+            <Tab menu={{ secondary: true, pointing: true }} panes={panes}></Tab>
+          </GridColumn>
+        </Grid>
+      </Container>
     </Layout>
   )
 }
