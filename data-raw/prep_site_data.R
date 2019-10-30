24e88cc37b0c571e89966759f118006ee1655041
sadie <sgill@flowwest.com>
2019-10-30 14:22:06 -0700

    new wq data

--- data-raw/prep_site_data.R
+++ data-raw/prep_site_data.R
@@ -2,31 +2,34 @@ library(readxl)
 library(tidyverse)
 library(jsonlite)
 
-excel_sheets('data-raw/Data_for_CEDEN_-_Sep_2018_to_Apr_2019.xlsx')
+excel_sheets('data-raw/WQ data.xlsx')
 
-habitat_results_raw <- read_excel('data-raw/Data_for_CEDEN_-_Sep_2018_to_Apr_2019.xlsx', 'HabitatResults')
+analytes <- read_excel('data-raw/WQ data.xlsx', 'analytes')
+units <- read_excel('data-raw/WQ data.xlsx', 'units') %>% 
+  rename(UnitName = Unit, UnitDescription = `Unit description`)
 
-glimpse(habitat_results_raw)
+observations_raw <- read_excel('data-raw/WQ data.xlsx', 'Observations')
 
-habitat_results_raw %>% 
-  select(StationCode, SampleDate, AnalyteName, VariableResult) %>% 
-  filter(VariableResult != "Not Recorded") %>% 
+glimpse(observations_raw)
+
+observations_raw %>% 
+  select(StationCode, SampleDate, AnalyteName, UnitName, Result = VariableResult) %>% 
+  filter(Result != "Not Recorded") %>% pull(AnalyteName) %>% unique
   group_by(StationCode, AnalyteName) %>% 
   nest() %>% 
   toJSON() #%>% 
   # write_json('wq-app-gatsby/src/data/habitat_data.json')
 
-field_results_raw <- read_excel('data-raw/Data_for_CEDEN_-_Sep_2018_to_Apr_2019.xlsx', 'FieldResults')
+field_results_raw <- read_excel('data-raw/WQ data.xlsx', 'Measurements')
 glimpse(field_results_raw)
 
 field_results_raw %>% 
   select(StationCode, SampleDate, AnalyteName, UnitName, Result) %>% 
-  mutate(AnalyteName = case_when(
-    AnalyteName == 'Oxygen, saturation' ~ 'Oxygen, Saturation',
-    AnalyteName == "SpecificConductivity" ~ "Specific Conductivity", 
-    TRUE ~ AnalyteName
-  )) %>% 
-  group_by(StationCode, AnalyteName, UnitName) %>% 
+  left_join(analytes) %>% 
+  left_join(units) %>% 
+  mutate(AnalyteName = Name) %>%
+  select(-Name) %>% 
+  group_by(StationCode, AnalyteName, UnitName, UnitDescription) %>% 
   nest() %>% 
   toJSON() #%>% 
 # write_json('wq-app-gatsby/src/data/field_data.json')
