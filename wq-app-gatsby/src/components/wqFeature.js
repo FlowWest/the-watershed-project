bdb86cddd5276f990abacbc7dd02c099c79f5496
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2019-11-01 00:39:04 -0500

    [add] water blob sytling for mouse events

--- wq-app-gatsby/src/components/wqFeature.js
+++ wq-app-gatsby/src/components/wqFeature.js
@@ -5,12 +5,17 @@ import ScrollModal from './scrollModal';
 
 const WQFeature = (props) => {
   const [isVisible, setVisibility] = useState(false);
-
+  const [ blobBackground, toggleBlobBackground] = useState(false)
   const toggleModal = () => setVisibility(!isVisible);
+  const mouseEnter = () => toggleBlobBackground(true);
+  const mouseLeave = () => toggleBlobBackground(false);
 
   return (
     <Fragment>
-      <div className={homeStyles.wqFeature} onClick={toggleModal}>
+      < div className = { blobBackground ? homeStyles.wqFeatureDark : homeStyles.wqFeature }
+        onClick={toggleModal}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}>
         <img className={homeStyles.waterDrop} src={waterDrop} />
         <h3 className={homeStyles.header}>{props.category}</h3>
       </div>
