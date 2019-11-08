fa509e3999ac30d20190df00faa3d205a9e1d991
sadie <sgill@flowwest.com>
2019-11-08 13:21:11 -0800

    add placeholder page for data download

---
+++ wq-app-gatsby/src/pages/download.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Download = () => {
+
+  return (
+    <Layout>
+      <SEO title="Download" />
+      <h1>Download</h1>
+    </Layout>
+  )
+}
+
+export default Download
