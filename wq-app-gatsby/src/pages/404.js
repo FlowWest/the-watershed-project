b954eee4739ad2d5608ee3f3068a35c9bd962128
sadie <sgill@flowwest.com>
2019-11-08 14:27:26 -0800

    customize 404 page

--- wq-app-gatsby/src/pages/404.js
+++ wq-app-gatsby/src/pages/404.js
@@ -1,13 +1,22 @@
 import React from "react"
-
+import { Container } from "semantic-ui-react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "../styles/404.module.css"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1 className={styles.header}>NOT FOUND</h1>
+      <p className={styles.paragraph}>
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
+      <p className={styles.paragraph}>
+        <Link to="/">Go home</Link>
+      </p>
+    </Container>
   </Layout>
 )
 
