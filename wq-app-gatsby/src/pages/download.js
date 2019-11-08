f7a02683ac1c63c001a342d0306a199a0eb4b137
sadie <sgill@flowwest.com>
2019-11-08 14:37:49 -0800

    add more to placeholders

--- wq-app-gatsby/src/pages/download.js
+++ wq-app-gatsby/src/pages/download.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Container } from "semantic-ui-react"
 
 const Download = () => {
-
   return (
     <Layout>
-      <SEO title="Download" />
-      <h1>Download</h1>
+      <Container>
+        <SEO title="Download" />
+        <h1>Download</h1>
+        <a href="https://the-watershed-project.s3-us-west-2.amazonaws.com/the-watershed-project-water-quality-data.xlsx">Download Water Quality Data</a>
+      </Container>
     </Layout>
   )
 }
