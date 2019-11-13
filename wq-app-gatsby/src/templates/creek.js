6d849772f74bbf660a1905381d3f9b6f129e9d13
sadie <sgill@flowwest.com>
2019-11-12 17:05:43 -0800

    reorg layout

--- wq-app-gatsby/src/templates/creek.js
+++ wq-app-gatsby/src/templates/creek.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql, navigate } from "gatsby"
 import Layout from "../components/layout"
 import creekImage from "../images/20180816_111841.jpg"
 import {
@@ -9,12 +9,12 @@ import {
   Container,
   Divider,
   Table,
-  TableHeader,
   TableRow,
   TableCell,
-  TableHeaderCell,
   TableBody,
-  Icon
+  Icon,
+  Dropdown,
+  GridRow,
 } from "semantic-ui-react"
 import Mapbox from "../components/creekMap"
 import creekStyles from "../styles/creek.module.css"
@@ -23,63 +23,85 @@ export default ({ data }) => {
   const creekData = data.allCreekSiteJson.edges[0].node
   const ptsNotFlat = data.allCreekSiteJson.edges.map(edge => edge.node.sites)
   const pts = [].concat(...ptsNotFlat)
-  
-  const colorLookUp = {Good: 'green', Marginal: 'yellow', Bad: 'red', NA: 'grey'}
+
+  const colorLookUp = {
+    Good: "green",
+    Marginal: "yellow",
+    Bad: "red",
+    NA: "grey",
+  }
   const analyteScores = data.allCreekScoresCsv.edges.map(edge => [
     edge.node.AnalyteName,
     edge.node.score,
   ])
 
+  const siteOptions = creekData.sites.map(site => ({
+    key: site.site_id,
+    text: `${site.name} (${site.site_id})`,
+    value: site.site_id,
+  }))
+
   return (
     <Layout>
       <Container>
         <Grid>
+          <GridColumn width={16}>
+            <h1 className={creekStyles.creekHeader}>{creekData.creek_name}</h1>
+          </GridColumn>
           <GridColumn width={6}>
-            <h1>{creekData.creek_name}</h1>
-            <Image src={creekImage} size="medium"></Image>
-            <h3>Associated Sites</h3>
-            <ul>
-              {creekData.sites.map((site, index) => {
-                return (
-                  <li key={index}>
-                    <Link to={`site/${site.site_id}`}>{site.name}</Link>
-                  </li>
-                )
-              })}
-            </ul>
+            <Image src={creekImage} size="large"></Image>
+            <Divider hidden />
+            <p className={creekStyles.creekDescription}>
+              {creekData.creek_description}
+            </p>
           </GridColumn>
           <GridColumn width={10}>
-            <Mapbox
-              pts={pts}
-              height={300}
-              zoom={11}
-              lat={creekData.creek_lat}
-              long={creekData.creek_long}
-            />
-            <Divider hidden />
-            <p>{creekData.creek_description}</p>
-            <div>
-              <h4>Creek Report Card</h4>
-              <Table celled padded>
-                <TableHeader>
-                  <TableRow>
-                    <TableHeaderCell>Vital Sign</TableHeaderCell>
-                    <TableHeaderCell>Score</TableHeaderCell>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {analyteScores.map(analyte => (
-                    <TableRow>
-                      <TableCell>{analyte[0]}</TableCell>
-                      <TableCell>
-                        <Icon name="circle" color={colorLookUp[analyte[1]]}></Icon>
-                        {`  ${analyte[1]}`}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
+            <Grid>
+              <GridRow>
+                <GridColumn width={16}>
+                  <Mapbox
+                    pts={pts}
+                    height={300}
+                    zoom={11}
+                    lat={creekData.creek_lat}
+                    long={creekData.creek_long}
+                  />
+                </GridColumn>
+              </GridRow>
+              <GridRow>
+                <GridColumn width={8}>
+                  <h3 className={creekStyles.header2}>Creek Report Card</h3>
+                  <Table basic="very" celled collapsing>
+                    <TableBody className={creekStyles.creekScore}>
+                      {analyteScores.map(analyte => (
+                        <TableRow>
+                          <TableCell>{analyte[0]}</TableCell>
+                          <TableCell>
+                            <Icon
+                              name="circle"
+                              color={colorLookUp[analyte[1]]}
+                            ></Icon>
+                            {`  ${analyte[1]}`}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </GridColumn>
+                <GridColumn width={8}>
+                  <h3 className={creekStyles.header2}>
+                    Explore Sampling Sites
+                  </h3>
+                  <Dropdown
+                    placeholder="Select Site"
+                    fluid
+                    selection
+                    options={siteOptions}
+                    onChange={(e, data) => navigate(`site/${data.value}`)}
+                  />
+                </GridColumn>
+              </GridRow>
+            </Grid>
           </GridColumn>
         </Grid>
       </Container>
