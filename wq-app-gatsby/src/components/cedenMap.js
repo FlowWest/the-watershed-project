104b1d3efd9d736884172b8476ca127816236fc3
sadie <sgill@flowwest.com>
2019-11-21 16:23:36 -0800

    start layout for ceden data

---
+++ wq-app-gatsby/src/components/cedenMap.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react"
+import ReactMapGL, { Marker, Popup } from "react-map-gl"
+import mapStyles from "../styles/map.module.css"
+import pin from "../images/marker-stroked-15.svg"
+import { navigate } from "gatsby"
+
+const TOKEN = process.env.GATSBY_MapboxAccessToken
+
+class MapboxCeden extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      viewport: {
+        width: "100%",
+        height: props.height,
+        latitude: props.lat,
+        longitude: props.long,
+        zoom: props.zoom,
+      },
+      selectedSite: null,
+    }
+  }
+
+  setSelectedSite = site => {
+    this.setState({
+      selectedSite: site
+    })
+  }
+
+  closePopup = () => {
+    this.setState({
+      selectedSite: null,
+    })
+  }
+
+  // mapStyle="mapbox://styles/mapbox/outdoors-v10?optimize=true"
+  render() {
+    return (
+      <ReactMapGL
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/mapbox/light-v9?optimize=true"
+        {...this.state.viewport}
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        {this.props.pts.map((pt, key) => (
+          <Marker latitude={pt.lat} longitude={pt.long} key={key}>
+            <img
+              className={mapStyles.locationIcon}
+              src={pin}
+              onMouseEnter={() => this.setSelectedSite(pt)}
+              // onClick={() => navigate(`/site/${pt.site_id}`)}
+            ></img>
+          </Marker>
+        ))}
+        {this.state.selectedSite !== null ? (
+          <Popup
+            latitude={this.state.selectedSite.lat}
+            longitude={this.state.selectedSite.long}
+            onClose={this.closePopup}
+            closeOnClick={false}
+          >
+            <div>
+              <p>{`${this.state.selectedSite.StationName}`}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
+    )
+  }
+}
+
+export default MapboxCeden
