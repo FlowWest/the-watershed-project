339cbbf31bf47f8461724bb6b140efcc280cf0d1
sadie <sgill@flowwest.com>
2019-11-25 09:28:20 -0800

    fix linter issue

--- wq-app-gatsby/src/templates/creek.js
+++ wq-app-gatsby/src/templates/creek.js
@@ -68,7 +68,7 @@ export default ({ data, pageContext }) => {
             <h1 className={creekStyles.creekHeader}>{creekData.creek_name}</h1>
           </GridColumn>
           <GridColumn width={6}>
-            {creekImage == defaultImage ? (
+            {creekImage === defaultImage ? (
               <Image src={creekImage} size="large"></Image>
             ) : (
               <a href={creekImage} target="_blank" rel="noopener noreferrer">
