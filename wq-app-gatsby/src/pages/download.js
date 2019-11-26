561350e976a1e17f644aee2a9340aed5ba9fa058
sadie <sgill@flowwest.com>
2019-11-26 15:10:39 -0800

    add ceden download link

--- wq-app-gatsby/src/pages/download.js
+++ wq-app-gatsby/src/pages/download.js
@@ -2,14 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Container } from "semantic-ui-react"
-import downloadStyles from '../styles/download.module.css'
+import downloadStyles from "../styles/download.module.css"
 
 const Download = () => {
   return (
     <Layout>
       <Container>
         <SEO title="Download" />
-        <h1 className={downloadStyles.header}>Download</h1>
+        <h1 className={downloadStyles.header}>Data Download</h1>
         <p className={downloadStyles.paragraph}>
           If you’d like to take a look at our data in its raw form, you can
           download it{" "}
@@ -17,6 +17,17 @@ const Download = () => {
             here
           </a>
           .
+        </p>
+        <p className={downloadStyles.paragraph}>
+          Please visit{" "}
+          <a
+            href="https://ceden.waterboards.ca.gov/AdvancedQueryTool"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CEDEN
+          </a>
+          {" "}to download their data.
         </p>
       </Container>
     </Layout>
